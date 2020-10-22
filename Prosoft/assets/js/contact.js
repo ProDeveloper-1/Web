a21508120609fb90a35f301f5f22ff789f4b0a77
ProDeveloper-1 <prospermbuma@gmail.com>
2020-10-23 02:06:56 +0300

    Advanced changes

--- Prosoft/assets/js/contact.js
+++ Prosoft/assets/js/contact.js
@@ -12,3 +12,16 @@ function myFocus() {
 function myBlur() {
     document.getElementById('password').style.border = ' 2px solid #1e90ff';
 }
+
+// Smooth Scroll
+$('.nav a').on('click', function (e) {
+    if (this.hash !== '') {
+        e.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 800);
+    }
+});
--- Prosoft/assets/js/index.js
+++ Prosoft/assets/js/index.js
@@ -12,3 +12,16 @@ function foCus_1() {
 let date = new (Date);
 let year = date.getFullYear();
 document.getElementById('year').innerHTML = year;
+
+// Smooth Scroll
+$('.nav a').on('click', function (e) {
+    if (this.hash !== '') {
+        e.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 800);
+    }
+});
