df5eaf2dd0ab199f5f5ee189024df34adc653242
ProDeveloper-1 <prospermbuma@gmail.com>
2020-11-11 10:25:09 +0300

    Further changes

--- Modern_JS/assets/js/index.js
+++ Modern_JS/assets/js/index.js
@@ -594,16 +594,49 @@ const inputs = document.querySelectorAll('input');
 
 inputs.forEach((input) => console.log(input));
 
-// Back To Top
+// = Back To Top = //
 const Top = document.querySelector('.go_top');
 
 window.addEventListener('scroll', () => {
-    if(window.pageYOffset > 100) {
+    if (window.pageYOffset > 100) {
         Top.classList.add('active');
     }
     else {
         Top.classList.remove('active');
     }
+
 });
+
+// = Navigation Menu = //
+
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-link-item');
+
+    burger.addEventListener('click', function () {
+        // Toggle Nav
+        nav.classList.toggle('nav-active');
+
+        // Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            }
+            else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+                // console.log(index / 7);
+                // console.log(index / 5 + 0.2);
+            }
+        });
+
+        // Burger Animation
+        burger.classList.toggle('toggle');
+
+    });
+
+}
+
+navSlide();
 
 // ==== End DOM ==== //
