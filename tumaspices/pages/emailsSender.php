1d6df20e9743adcf8a794119318b5d7dc612d767
Prodeveloper1 <prodeveloper1@localhost.localdomain>
2021-01-19 01:47:38 +0300

    Changed something

--- tumaspices/pages/emailsSender.php
+++ tumaspices/pages/emailsSender.php
@@ -22,7 +22,7 @@ if (isset($_POST['name']) && isset($_POST['email'])) {
     $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
     $mail->Username   = 'prospermbuma@gmail.com';                     // SMTP username (Gmail address that you will use as a SMTP server)
-    $mail->Password   = 'ihrqcylmkqdoyokt';                               // SMTP password
+    $mail->Password   = '';                               // SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     //$mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
     $mail->SMTPSecure = "ssl";
